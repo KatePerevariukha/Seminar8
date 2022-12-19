502de130c9d723cbcfebec15d6a79d231d2e464d
Kate <Kate-pretty@ya.ru>
2022-12-20 00:12:05 +0500

    added files

---
+++ 60/Program.cs
@@ -0,0 +1,70 @@
+﻿int InputNum (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+
+int [,,] SetArray3DRandom (int row, int line, int column)
+{
+    int start = 10;
+    int [] number = new int [90];
+    for (int i = 0; i < 90; i++)
+    {
+        number [i] = start++;
+    }
+
+    for (int i = 0; i < 90; i++)
+    {
+        int idx = new Random ().Next (0, 90);
+        int tempNmr = number [idx];
+        number [idx] = number [i];
+        number [i] = tempNmr;
+    }
+
+    int nbrIdx = 0;
+    int [,,] numArr3D = new int [row, line, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < line; j++)
+        {
+            for (int k = 0; k < column; k++)
+            {
+                numArr3D [i, j, k] = number [nbrIdx++];
+            }
+        }
+    }
+    return numArr3D;
+}
+
+void PrintArray3D (int [,,] array3D) 
+{
+    for (int i = 0; i < array3D.GetLength (0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength (1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength (2); k++)
+            {
+                Console.Write ($"{array3D [i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine ();
+        }
+        Console.WriteLine ();
+    }
+}
+
+int k = InputNum ("Количество рядов массива: ");
+int m = InputNum ("Количество строк массива: ");
+int n = InputNum ("Количество столбцов массива: ");
+Console.WriteLine ();
+
+if (k*m*n > 90)
+{
+    Console.WriteLine ("Размерность массива превышает допустимую, возможны повторения чисел!");
+    return;
+}
+
+int [,,] array3D = SetArray3DRandom (k, m, n);
+
+Console.WriteLine ($"Трехмерный массив с индексами элементов:");
+PrintArray3D (array3D);
---
+++ 62/Program.cs
@@ -0,0 +1,48 @@
+﻿int InputNum (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+int [,] SpiralArray (int side)
+{
+    int number = 1;
+    int i = 0;
+    int j = 0;
+    int [,] spiralArr = new int [side, side];
+    while (number <= side*side)
+    {
+        spiralArr [i, j] = number++;
+        if (i <= j + 1 && i + j < side - 1)
+            j++;
+        else if (i < j && i + j >= side - 1)
+            i++;
+        else if (i >= j && i + j > side - 1)
+            j--;
+        else
+            i--;
+    }
+    return spiralArr;
+}
+
+void PrintArray2D (int [,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength (0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength (1); j++)
+        {
+            if (array2D [i, j] < 10)
+            Console.Write ($"0{array2D [i, j]} ");
+            else
+            Console.Write ($"{array2D [i, j]} ");
+        }
+        Console.WriteLine ();
+    }
+}
+
+int n = InputNum ("Задайте сторону n массива nxn: ");
+
+int [,] array = SpiralArray (n);
+
+Console.WriteLine ($"Спиральный массив {n}x{n} :");
+PrintArray2D (array);
